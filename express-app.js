e67bdc5a86487d8da30a3a78b745be325d2e49ec
Soumil Maitra <soumilmaitra414@gmail.com>
2023-01-25 15:51:00 +0530

    chore: remove body-parser from express server

--- express-app.js
+++ express-app.js
@@ -1,9 +1,7 @@
 const express=require("express");
 const app=express();
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 let taskArray=[];
 let id=0;
